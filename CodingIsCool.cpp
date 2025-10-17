b4a7444d82d0218916607a757ea48127bdc776b6
Sambubu-hash <2409-122860@esl.ac.uk>
2025-10-17 11:44:31 +0100

    Add files via upload

---
+++ CodingIsCool.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // to accessing cout and cin 
+using namespace std;// to avoid repeating std:: when calling standard libaries
+ 
+int main(){  // defining main function 
+    cout << "Coding is cool" << endl; // It prints "Coding is cool"
+ 
+    return 0;
+}
