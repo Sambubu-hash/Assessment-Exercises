17ae6fd01539a33f31502fbcfe6848f5191e2010
Sambubu-hash <2409-122860@esl.ac.uk>
2025-10-31 11:02:46 +0000

    Add files via upload

---
+++ Biography.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std; 
+
+int main(){
+
+    string Name = "Daniel Silva";
+    string HomeTown = "Baixa da Banheira";
+    int Age = 199;
+
+    cout <<"Name: "<< Name << endl;
+    cout <<"HomeTown: "<< HomeTown << endl;
+    cout << "Age: " << Age << endl;
+
+
+    return 0;
+}
